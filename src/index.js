55d2a1e327b843fdd0f2a36f0fc752a1bb621712
Cole Kubossek <cole.kubossek@gmail.com>
2021-04-13 09:07:16 -0700

    fixes missing application.js data

--- src/index.js
+++ src/index.js
@@ -7,32 +7,3 @@ import Application from "components/Application";
 import DayList from "components/DayList";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-storiesOf("DayList", module)
-  .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
-  })
-  .add("Monday", () => (
-    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
-  ))
-  .add("Tuesday", () => (
-    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
-  ));
