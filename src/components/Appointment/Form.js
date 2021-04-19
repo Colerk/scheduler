5756a3968cc34b31f1d1e2a452c627cc319aa322
Cole Kubossek <cole.kubossek@gmail.com>
2021-04-19 16:17:51 -0700

    passes tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -10,6 +10,7 @@ export default function Form(props) {
 
   const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName('')
@@ -20,10 +21,15 @@ export default function Form(props) {
     props.onCancel();
   }
 
-  const save = () => {
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("")
     props.onSave(name, interviewer);
   }
-
+  
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -36,18 +42,17 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={name}
             onChange={event => {setName(event.target.value)}}
-            /*
-              This must be a controlled component
-            */
+            data-testid="student-name-input"
           />
         </form>
+          <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} 
           setInterviewer={setInterviewer} />      
         </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={save} onSubmit={event => event.preventDefault()} confirm>Save</Button>
+          <Button onClick={validate} confirm>Save</Button>
         </section>
       </section>
     </main>
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,22 +1,80 @@
-/*
-  We are rendering `<Application />` down below, so we need React.createElement
-*/
 import React from "react";
 
-/*
-  We import our helper functions from the react-testing-library
-  The render function allows us to render Components
-*/
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
 
-/*
-  We import the component that we are testing
-*/
-import Application from "components/Application";
+import Form from "components/Appointment/Form";
 
-/*
-  A test that renders a React Component
-*/
-it("renders without crashing", () => {
-  render(<Application />);
+afterEach(cleanup);
+
+
+describe("Form", () => {
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    }
+  ];
+
+
+  it("renders without student name if not provided", () => {
+  const fn = jest.fn();
+
+    const { getByPlaceholderText } = render(
+      <Form interviewers={interviewers} />
+    );    
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  });
+
+
+
+  it("renders with initial student name", () => {
+    const { getByTestId } = render(
+      <Form interviewers={interviewers} name="Lydia Miller-Jones" />
+    );    
+    expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  });
+
+
+
+  it("validates that the student name is not blank", () => {
+    const onSave = jest.fn();
+
+    const { queryByText, getByText } = render(
+      <Form interviewers={interviewers} name="" onSave={onSave}/>
+    );  
+    fireEvent.click(getByText("Save"));
+
+    /* 1. validation is shown */
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+  
+    /* 2. onSave is not called */
+    expect(onSave).not.toHaveBeenCalled();
+
+  });
+  
+
+
+  it("calls onSave function when the name is defined", () => {
+    const onSave = jest.fn();
+
+    const { queryByText, getByText } = render(
+      <Form interviewers={interviewers} name="Lydia Miller-Jones" onSave={onSave}/>
+    );  
+    /* 3. validation is not shown */
+
+    fireEvent.click(getByText("Save"));
+
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    
+    /* 4. onSave is called once*/
+    expect(onSave).toHaveBeenCalledTimes(1);
+  
+    /* 5. onSave is called with the correct arguments */
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
+  
+
+
 });
